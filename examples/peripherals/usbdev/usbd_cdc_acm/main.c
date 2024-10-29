fd322d3245c9a2d542d83fa92cb36faf348f7283
Dozingfiretruck <1473454180@qq.com>
2024-10-29 12:13:54 +0800

    update: cherryusb to 1.4.1

--- examples/peripherals/usbdev/usbd_cdc_acm/main.c
+++ examples/peripherals/usbdev/usbd_cdc_acm/main.c
@@ -1,17 +1,37 @@
+#include <FreeRTOS.h>
+#include "semphr.h"
 #include "usbd_core.h"
-#include "bflb_mtimer.h"
 #include "board.h"
 
-extern void cdc_acm_init(void);
+#define DBG_TAG "MAIN"
+#include "log.h"
+
+extern void cdc_acm_init(uint8_t busid, uintptr_t reg_base);
 extern void cdc_acm_data_send_with_dtr_test(void);
+
+static TaskHandle_t usb_handle;
+
+static void usb_task(void *pvParameters)
+{
+
+    LOG_I("Starting usb host task...\r\n");
+
+    cdc_acm_init(0, 0x20072000);
+    while (1) {
+        cdc_acm_data_send_with_dtr_test();
+        bflb_mtimer_delay_ms(500);
+    }
+
+    vTaskDelete(NULL);
+}
 
 int main(void)
 {
     board_init();
 
-    cdc_acm_init();
+    xTaskCreate(usb_task, (char *)"usb_task", 512, NULL, configMAX_PRIORITIES - 3, &usb_handle);
+
+    vTaskStartScheduler();
     while (1) {
-        cdc_acm_data_send_with_dtr_test();
-        bflb_mtimer_delay_ms(500);
     }
 }
